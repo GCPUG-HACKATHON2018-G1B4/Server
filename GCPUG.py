5cda71a734248f07a3258d89639c4de5a1c5cd91
LikeJust <diddydwns135@gmail.com>
2018-03-31 05:13:33 +0900

    tag, code input

--- GCPUG.py
+++ GCPUG.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World! \n'
+@app.route('/setting')
+def tag():
+    tag_dict = dict(request.args)['tag']
+    print(tag_dict)
+    return str(tag_dict)
+
+
+@app.route('/realtime')
+def real_time():
+    code_str = request.args.get('code')
+    print(code_str)
+    return code_str
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
